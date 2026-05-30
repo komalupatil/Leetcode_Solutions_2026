6a4eabd3c7fd0d3969043b4cc562cd60da6b8853
komalupatil <ku.patil31@gmail.com>
2026-05-30 00:56:38 -0700

    Adding solution for leetcode 3880 Minimum Absolute Difference between two values

---
+++ Easy/Minimum_Absolute_Difference_Between_Two_Values.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/minimum-absolute-difference-between-two-values/description/
+
+class Solution:
+    def minAbsoluteDifference(self, nums: list[int]) -> int:
+        idx1 = -1
+        idx2 = -1
+        res = float('inf')
+
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                idx1 = i
+                if idx2 != -1:
+                    res = min(abs(idx2-idx1), res)
+            elif nums[i] == 2:
+                idx2 = i
+                if idx1 != -1:
+                    res = min(abs(idx1-idx2), res)
+        return -1 if res == float('inf') else res
